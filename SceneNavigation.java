1efce41d25051ce32039ff6e54cc27d3b7cc1b6b
ryntt <76274559+ryntt@users.noreply.github.com>
2022-11-05 00:11:30 -0700

    Add files via upload

--- SceneNavigation.java
+++ SceneNavigation.java
@@ -1,16 +1,22 @@
 package application;
 
+import java.util.ArrayList;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -43,14 +49,16 @@ public class SceneNavigation {
 		
 		//login button
 		Button loginBtn = new Button("Login");
-		loginBtn.setStyle("-fx-background-color: #e6d1f2;" + "-fx-cursor: hand;");
+		loginBtn.setStyle("-fx-background-color: #FFFAEA;" + "-fx-cursor: hand;");
 		loginBtn.setOnAction(event -> switchToCourses(primary));
 		
 		//links for signing up for a new account and resetting password
 		Hyperlink signupBtn = new Hyperlink("Don't have an account? Click here");
 		signupBtn.setOnAction(event -> switchToSignup(primary));
+		signupBtn.setStyle("-fx-text-fill: #ffffff");
 		Hyperlink resetBtn = new Hyperlink("Forgot your password?");
 		resetBtn.setOnAction(event -> switchToReset(primary));
+		resetBtn.setStyle("-fx-text-fill: #ffffff");
 				
 		//user-name and password text fields
 		//extra lambda expressions written to allow login by pressing return key
@@ -68,7 +76,7 @@ public class SceneNavigation {
 				}
 			}
 		});
-		
+
 		passField.setOnKeyPressed(event -> {
 			if (event.getCode() == KeyCode.ENTER) {
 				if (!userField.getText().isEmpty() && !passField.getText().isEmpty()) {
@@ -79,7 +87,7 @@ public class SceneNavigation {
 		
 		//styling the VBox pane where everything goes
 		VBox root = new VBox();
-		root.setStyle("-fx-background-color: #dfe9f5");
+		root.setStyle("-fx-background-color: linear-gradient(to top,#6299F9, #FFFAEA)");
 		root.setPadding(new Insets(100, 1, 100, 105));
 		root.setSpacing(10);
 		root.getChildren().add(nameLbl);
@@ -122,7 +130,7 @@ public class SceneNavigation {
 		
 		//styling the layout pane and adding components
 		GridPane reset = new GridPane();
-		reset.setStyle("-fx-background-color: #dfe9f5");
+		reset.setStyle("-fx-background-color: linear-gradient(to top,#6299F9, #FFFAEA)");
 		reset.setPadding(new Insets(100, 100, 100, 120));
 		reset.setVgap(20);
 		reset.setHgap(10);
@@ -186,7 +194,7 @@ public class SceneNavigation {
 		
 		//styling the layout pane and adding components
 		GridPane signup = new GridPane();
-		signup.setStyle("-fx-background-color: #dfe9f5");
+		signup.setStyle("-fx-background-color: linear-gradient(to top,#6299F9, #FFFAEA)");
 		signup.setPadding(new Insets(100, 100, 100, 85));
 		signup.setVgap(20);
 		signup.setHgap(10);
@@ -209,18 +217,87 @@ public class SceneNavigation {
 	
 	//switches the scene to the main course page
 	private void switchToCourses(Stage primary) {
+		ArrayList<Course> listCourses = new ArrayList<Course>();
+		ArrayList<Button> listButtons = new ArrayList<Button>();
 		primary.setTitle("My Courses");
-		Hyperlink returnToMain = new Hyperlink("Return to login");
+		// logout button and enter
+		Button returnToMain = new Button("Logout");
 		returnToMain.setOnAction(event -> switchToMain(primary));
-		VBox signup = new VBox();
-		signup.getChildren().add(returnToMain);
+		Button enter = new Button("Enter");
+		Button delete = new Button("Delete");
+		returnToMain.setStyle("-fx-text-fill: #FFFFFF;" + "-fx-background-color: #AFBAF7;" 
+									+"-fx-font-size: 13;" + "-fx-cursor: hand;");
+		enter.setStyle("-fx-text-fill: #FFFFFF;" + "-fx-background-color: #0038A8;" 
+				+"-fx-font-size: 13;" + "-fx-cursor: hand;");	
+		delete.setStyle("-fx-text-fill: #FFFFFF;" + "-fx-background-color: #FF6A6A;" 
+				+"-fx-font-size: 13;" + "-fx-cursor: hand;");
+		Label name = new Label("Enter Name");
+		//set window look
+		name.setFont(new Font("Georgia", 15));
+		FlowPane signup = new FlowPane();
+		signup.setVgap(10);
+		signup.setHgap(10);
+		signup.setOrientation(Orientation.HORIZONTAL);
+		signup.getChildren().add(name);
+		signup.setMargin(name, new Insets(20, 20, 20, 20));
+		
+		TextField text = new TextField();
+		
+		text.setMaxWidth(200);
+		
+		// adds a new course 
+		
+		enter.setOnAction(event -> {
+			String enterName = text.getText();
+			if (!enterName.isEmpty()) {
+				Button newCourse = new Button(enterName);
+				newCourse.setStyle("-fx-text-fill: #000000;" + "-fx-background-color: #F5DE9C;" 
+						+ "-fx-border-color: #EBC861; -fx-border-width: 2px;" 
+						+ "-fx-cursor: hand;");
+				newCourse.setMinSize(100, 60);
+				signup.getChildren().add(newCourse);
+				listCourses.add(new Course(enterName));
+				listButtons.add(newCourse);
+				text.clear();
+				ContextMenu menu = new ContextMenu();
+				MenuItem deleteBtn = new MenuItem("Delete");
+				MenuItem renameBtn = new MenuItem("Rename");
+				menu.getItems().addAll(deleteBtn, renameBtn);
+				newCourse.setContextMenu(menu);
+				deleteBtn.setOnAction(e -> signup.getChildren().remove(newCourse));
+				renameBtn.setOnAction(f -> {
+					TextField renaming = new TextField();
+					signup.getChildren().add(renaming);
+					String newText = renaming.getText();
+					newCourse.setText("");
+					newCourse.setGraphic(renaming);
+					renaming.setOnKeyPressed(g -> {
+						if (g.getCode() == KeyCode.ENTER) {
+							newCourse.setText(newText);
+							newCourse.setGraphic(null);
+						}
+					});
+				});
+			}
+		});
+		
+		
+		//set up the scene
 		Scene scene1 = new Scene(signup, 550, 550);
+		// sets background
 		scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		signup.getChildren().addAll(text, enter, delete, returnToMain);
+		signup.setStyle("-fx-background-color: linear-gradient(to top,#6299F9, #FFFAEA)");
+		
+		
 		primary.setScene(scene1);
+		
+		
 	}
 	
 	//runs the app. encapsulation is used to ensure the user cannot
 	//see the other methods and only knows to run the program.
+	
 	public void runApp(Stage primary) {
 		switchToMain(primary);
 	}
